9c71b405abd7085166a2736ba202ef2ae239fdd9
victfung-dev <victfung@gmail.com>
2025-07-25 15:32:53 +0800

    Commit Proposal

--- contracts/Proposal.sol
+++ contracts/Proposal.sol
@@ -5,10 +5,13 @@ pragma solidity ^0.8.18;
 contract ProposalContract {
     // ****************** Data ***********************
 
+
     //Owner
     address owner;
 
+
     uint256 private counter;
+
 
     struct Proposal {
         string description; // Description of the proposal
@@ -20,9 +23,12 @@ contract ProposalContract {
         bool is_active; // This shows if others can vote to our contract
     }
 
+
     mapping(uint256 => Proposal) proposal_history; // Recordings of previous proposals
 
+
     address[] private voted_addresses; 
+
 
     //constructor
     constructor() {
@@ -30,59 +36,49 @@ contract ProposalContract {
         voted_addresses.push(msg.sender);
     }
 
+
     modifier onlyOwner() {
         require(msg.sender == owner);
         _;
     }
+
 
     modifier active() {
         require(proposal_history[counter].is_active == true);
         _;
     }
 
+
     modifier newVoter(address _address) {
-        require(!isVoted(_address), "Address has already voted");
+        require(!isVoted(_address), "Address has not voted yet");
         _;
     }
 
 
-//     // ****************** Execute Functions ***********************
+    // ****************** Execute Functions ***********************
+
+
 
 
     function setOwner(address new_owner) external onlyOwner {
         owner = new_owner;
     }
 
+
     function create(string calldata _description, uint256 _total_vote_to_end) external onlyOwner {
         counter += 1;
         proposal_history[counter] = Proposal(_description, 0, 0, 0, _total_vote_to_end, false, true);
     }
     
-    function calculateCurrentState() private view returns(bool) {
-    Proposal storage proposal = proposal_history[counter];
 
-    uint256 approve = proposal.approve;
-    uint256 reject = proposal.reject;
-    uint256 pass = proposal.pass;
-        
-    if (proposal.pass %2 == 1) {
-        pass += 1;
-    }
-
-    pass = pass / 2;
-
-    if (approve > reject + pass) {
-        return true;
-    } else {
-        return false;
-    }
-}
 
     function vote(uint8 choice) external active newVoter(msg.sender){
         Proposal storage proposal = proposal_history[counter];
         uint256 total_vote = proposal.approve + proposal.reject + proposal.pass;
 
+
         voted_addresses.push(msg.sender);
+
 
         if (choice == 1) {
             proposal.approve += 1;
@@ -95,9 +91,68 @@ contract ProposalContract {
             proposal.current_state = calculateCurrentState();
         }
 
+
         if ((proposal.total_vote_to_end - total_vote == 1) && (choice == 1 || choice == 2 || choice == 0)) {
             proposal.is_active = false;
             voted_addresses = [owner];
         }
+    }
+
+
+    function terminateProposal() external onlyOwner active {
+        proposal_history[counter].is_active = false;
+    }
+
+
+
+
+    function calculateCurrentState() private view returns(bool) {
+        Proposal storage proposal = proposal_history[counter];
+
+
+        uint256 approve = proposal.approve;
+        uint256 reject = proposal.reject;
+        uint256 pass = proposal.pass;
+        
+        if (proposal.pass %2 == 1) {
+            pass += 1;
+        }
+
+
+        pass = pass / 2;
+
+
+        if (approve > reject + pass) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+
+
+    // ****************** Query Functions ***********************
+
+
+    function isVoted(address _address) public view returns (bool) {
+        for (uint i = 0; i < voted_addresses.length; i++) {
+            if (voted_addresses[i] == _address) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+
+
+    function getCurrentProposal() external view returns(Proposal memory) {
+        return proposal_history[counter];
+    }
+
+
+    function getProposal(uint256 number) external view returns(Proposal memory) {
+        return proposal_history[number];
     }
 }
